ccf71deefc5d1d6fdc0d4ab15e32d532f24166cc
shine_5402 <shine_5402@live.cn>
2022-01-10 03:50:31 +0800

    add about and icon resource

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication::setStyle("Fusion");
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icon/appIcon"));
     MainWindow w;
     w.show();
     return a.exec();
