6c14a092da685d7be23e17dbe7974efa30adccd1
shine_5402 <shine_5402@live.cn>
2022-01-10 01:08:01 +0800

    Use Fusion for Style

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
+    QApplication::setStyle("Fusion");
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
